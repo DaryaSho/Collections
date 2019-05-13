a1657a3aca9d4eb2a766dd10e1e1c12c16d4cc45
Дарья Жоголь <da18121996@yandex.ru>
2019-05-13 16:46:26 +0300

    Added Linked list with functions Add and Remove

---
+++ Task1/MyLinkedList.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class MyLinkedList<T>
+    {
+        private Node first;
+        private Node last;
+        private int number;
+
+        public MyLinkedList()
+        {
+            first = null;
+            last = null;
+            number = 0;
+        }
+
+        public void add(T item)
+        {
+            if (!isEmpty())
+            {
+                Node previous = last;
+                last = new Node(item, null);
+                previous.next = last;
+            }
+            else
+            {
+                last = new Node(item, null);
+                first = last;
+            }
+            number++;
+        }
+
+        public bool remove(T item)
+        {
+            if (isEmpty()) return false;
+            bool result = false;
+            Node previous = first;
+            Node current = first;
+            while (current.next != null || current == last)
+            {
+                if (current.value.Equals(item))
+                {
+                    if (number == 1) { first = null; last = null; }
+                    else if (current.Equals(first)) { first = first.next; }
+                    else if (current.Equals(last)) { last = previous; last.next = null; }
+                    else { previous.next = current.next; }
+                    number--;
+                    result = true;
+                    break;
+                }
+                previous = current;
+                current = previous.next;
+            }
+            return result;
+        }
+
+        public int count()
+        {
+            return number;
+        }
+
+        public bool isEmpty()
+        {
+            return number == 0;
+        }
+
+        private class Node
+        {
+            public T value;
+            public Node next;
+
+            public Node(T value, Node next)
+            {
+                this.value = value;
+                this.next = next;
+            }
+        }
+
+    }
+}
