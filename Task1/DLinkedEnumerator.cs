419b9b450bd0ab2775451571902ea0f66be95dfa
Дарья Жоголь <da18121996@yandex.ru>
2019-05-14 15:45:26 +0300

    Added Enumerator for DlinkedList

---
+++ Task1/DLinkedEnumerator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class DLinkedEnumerator<T>:IEnumerator
+    {
+         Node<T> first;
+         Node<T> last;
+         Node<T> current;
+        bool number = true;
+
+
+        public DLinkedEnumerator(Node<T> first, Node<T> last)
+        {
+            this.first = first;
+            this.last = last;
+            current = first;
+            
+        }
+
+        public bool MoveNext()
+        {
+            if (number)
+            {
+                number = false;
+                return true;
+            }
+            current = current.next;
+            return current != null;
+        }
+
+        public void Reset()
+        {
+            current = first;
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public T Current
+        {
+            get
+            {
+                try
+                {
+                    return current.value;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
